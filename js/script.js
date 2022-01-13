32f03874623c5676aa008efd67b3f83dd3219f24
Ali Shaikh <31328423+AliNAU2020@users.noreply.github.com>
2022-01-13 02:04:15 -0600

    included more files like img, js and css

---
+++ js/script.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+
+    //AOS (Animate on Scroll)
+    AOS.init();
+
+    //NavBar Toggling
+    $('#navbar-toggler').click(function(){
+        $('navbar-collapse').slideToggle();
+    });
+
+    //NavBar Background Change in Scroll
+    $(window).scroll(function(){
+        let position = $(window).scrollTop();
+        if(position > 80){
+            $('.navbar').addClass('bg-navbar');
+        }
+        else{
+            $('.navbar').removeClass('bg-navbar');
+        }
+    });
+
+    //Smooth Scroll
+    let links = $('.navbar-nav a.nav-link');
+    links.click(function(e){
+        e.preventDefault();
+        let target = $(this).attr('href');
+        $('html, body').stop().animate({
+            scrollTop: $(target).offset().top
+        }, 700);
+    });
+
+    //LightBox Gallery (Popup Gallery)
+    $(document).ready(function() {
+        $('.popup-gallery').magnificPopup({
+            delegate: 'a',
+            type: 'image',
+            tLoading: 'Loading image #%curr%...',
+            mainClass: 'mfp-img-mobile',
+            gallery: {
+                enabled: true,
+                navigateByImgClick: true,
+                preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+            },
+            image: {
+                tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+                titleSrc: function(item) {
+                    return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+                }
+            }
+        });
+    });
+
+    // Slick Slider 
+    $('.testimonial-slider').slick({
+        dots: true,
+        infinite: true,
+        autoplay: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true
+    });
+});
